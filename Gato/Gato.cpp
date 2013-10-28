e6f9cca6ce18a56b3a1b8535d652547cbccb4282
ramontiveros <rontiveros@sitek.mx>
2013-10-27 19:52:02 -0700

    Update Gato.cpp
    Cambio a uso de arreglos y matrices

--- Gato/Gato.cpp
+++ Gato/Gato.cpp
@@ -9,8 +9,9 @@ using namespace std;
 const int EMPTY = 0;
 const int OS = 1;
 const int XS = 2;
+const int wins[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0,3,6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}}; 
 
-int G1 = EMPTY, G2 = EMPTY, G3 = EMPTY, G4 = EMPTY, G5 = EMPTY, G6 = EMPTY, G7 = EMPTY, G8 = EMPTY, G9 = EMPTY;
+int dashboard[9] = {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY};
 bool xplay = true;
 
 int requestPlay()
@@ -34,94 +35,47 @@ int getPlayNumber()
 
 bool assignPlay(int play)
 {
-	switch(play)
-	{
-		case 1:
-			if (G1 == EMPTY) {
-				G1 = getPlayNumber();
-				return true;
-			}
-			break;
-		case 2:
-			if (G2 == EMPTY) {
-				G2 = getPlayNumber();
-				return true;
-			};
-			break;
-		case 3:
-			if (G3 == EMPTY) {
-				G3 = getPlayNumber();
-				return true;
-			}
-			break;
-		case 4:
-			if (G4 == EMPTY) {
-				G4 = getPlayNumber();
-				return true;
-			}
-			break;
-		case 5:
-			if (G5 == EMPTY) {
-				G5 = getPlayNumber();
-				return true;
-			}
-			break;
-		case 6:
-			if (G6 == EMPTY) {
-				G6 = getPlayNumber();
-				return true;
-			}
-			break;
-		case 7:
-			if (G7 == EMPTY) {
-				G7 = getPlayNumber();
-				return true;
-			}
-			break;
-		case 8:
-			if (G8 == EMPTY) {
-				G8 = getPlayNumber();
-				return true;
-			}
-			break;
-		case 9:
-			if (G9 == EMPTY) {
-				G9 = getPlayNumber();
-				return true;
-			}
-			break;
-		default:
-			cout << "El numero " << play << " no es parte del tablero de juego";
+	if(play >= 1 && play <= 9) { 
+		if (dashboard[play-1] == EMPTY) {
+			dashboard[play-1] = getPlayNumber();
+			return true;
+		} else 
+			cout << "La casilla " << play << " ya esta ocupada. ";
 			return false;
-			break;
+	} else {
+		cout << "El numero " << play << " no es parte del tablero de juego. ";
+		return false;
+	}
+}
+
+bool win(int player)
+{
+	for(int i=0;i<8;i++) {
+		bool test = true;
+		for(int j=0;j<3;j++)
+			test = test && (dashboard[wins[i][j]] == player);
+		if (test)
+			return true;
 	}
 	return false;
 }
 
-
-bool xwin()
-{
-	return (G1 == XS && G2 == XS && G3 == XS) || (G4 == XS && G5 == XS && G6 == XS) || (G7 == XS && G8 == XS && G9 == XS) || (G1 == XS && G5 == XS && G7 == XS) || (G2 == XS && G5 == XS && G8 == XS) || (G3 == XS && G6 == XS && G9 == XS) || (G1 == XS && G5 == XS && G9 == XS) || (G3 == XS && G5 == XS && G7 == XS);
-}
-
-bool owin()
-{
-	return (G1 == OS && G2 == OS && G3 == OS) || (G4 == OS && G5 == OS && G6 == OS) || (G7 == OS && G8 == OS && G9 == OS) || (G1 == OS && G5 == OS && G7 == OS) || (G2 == OS && G5 == OS && G8 == OS) || (G3 == OS && G6 == OS && G9 == OS) || (G1 == OS && G5 == OS && G9 == OS) || (G3 == OS && G5 == OS && G7 == OS);
-}
-
 bool tie()
 {
-	return (G1 != EMPTY && G2 != EMPTY && G3 != EMPTY && G4 != EMPTY && G5 != EMPTY && G6 != EMPTY && G7 != EMPTY && G8 != EMPTY && G9 != EMPTY);
+	for(int i=0; i<9; i++) 
+		if(dashboard[i] == EMPTY)
+			return false;
+	return true;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {	
-	while(!(xwin() || owin() || tie())) {
+	while(!(win(XS) || win(OS) || tie())) {
 		while (!assignPlay(requestPlay()));
 		xplay = !xplay;
-		if (xwin())
+		if (win(XS))
 			cout << "Felicidades x a gando. Weeee!!" << endl;
-		if (owin())
+		if (win(OS))
 			cout << "Felicidades o a gando. Weeee!!" << endl;
 		if (tie())
 			cout << "Juego empatado";
